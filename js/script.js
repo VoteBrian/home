b6219b58a79ddedecbb2866a25fe16270e6d4df7
Brian <bflores@gmail.com>
2012-05-13 19:27:51 -0400

    random circles on background canvas

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ function drawBG() {
     if(bgCanvas.getContext) {
         var ctx = bgCanvas.getContext('2d');
 
+        var colors = new Array();
+        colors[0] = "#615046";
+        colors[1] = "#44503A";
+        colors[2] = "#9BA29A";
+        colors[3] = "#39455F";
+        colors[4] = "#44474E";
+
+        bgClr = "#F4F4F4";
+
         w = window.innerWidth;
         h = window.innerHeight;
 
@@ -12,16 +21,47 @@ function drawBG() {
         ctx.fillStyle = "rgba(244, 244, 244, 1.0)";
         ctx.fillRect(0, 0, w, h);
 
-        var gradTL = ctx.createRadialGradient(0,0,0, 0,0,h);
-        gradTL.addColorStop(0, 'rgba(131, 148, 172, 0.75)');
-        gradTL.addColorStop(1, 'rgba(244, 244, 244, 0)');
-        ctx.fillStyle = gradTL;
-        ctx.fillRect(0, 0, w, h);
+        cntX = 0;
+        cntY = 0;
+        rad = 0;
 
-        var gradBR = ctx.createRadialGradient(w,0,0, w,0,h);
-        gradBR.addColorStop(0, 'rgba(131, 148, 172, 0.75)');
-        gradBR.addColorStop(1, 'rgba(244, 244, 244, 0)');
-        ctx.fillStyle = gradBR;
-        ctx.fillRect(0, 0, w, h);
+        for(x=0; x < 50; x++) {
+            prvX = cntX;
+            prvY = cntY;
+            prvR = rad;
+
+            if(Math.floor(Math.random()*2)) {
+                signX = 1;
+            } else signX = -1;
+
+            if(Math.floor(Math.random()*2)) {
+                signY = 1;
+            } else signY = -1;
+
+            rad = Math.floor(Math.random()*150)+20;
+            maxDev = rad+prvR;
+
+            cntX = prvX + (signX * Math.floor(Math.random()*maxDev));
+            if(cntX < 0) {
+                cntX = 0;
+            } else if(cntX > w) {
+                cntX = w;
+            }
+            cntY = prvY + (signY * Math.floor(Math.random()*maxDev));
+            if(cntY < 0) {
+                cntY = 0;
+            } else if(cntY > h) {
+                cntY = h;
+            }
+            clr = colors[Math.floor(Math.random()*5)];
+
+            ctx.beginPath();
+            ctx.arc(cntX, cntY, rad, 0, 2* Math.PI, false);
+            ctx.fillStyle=clr;
+            ctx.fill();
+            ctx.lineWidth=5;
+            ctx.strokeStyle=bgClr;
+            ctx.stroke();
+        }
     }
 }
